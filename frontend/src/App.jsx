e08cca1454f94c3da233f7e0e7e3d1d9aadc53fb
lucasgruenwald <lgruenwald@sbcglobal.net>
2022-07-04 22:29:50 -0500

    add h1 for seo and adjust styling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
 
       <div className='body-container'>
         <div className='about-section' id="about">
-          <h3 className="about-us-h3">We Get Excited to Work</h3>
+          <h1 className="about-us-h1">We Get Excited to Work</h1>
           <p className="about-us-p">At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. <br/><br/> Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.</p>
         </div>
         <div className='values-section body-slice-primary' id="values">
